9df2a72636be47d77e97e6ba3439569a7eab5167
Halil Ömer Öksüz <omeroksz6128@gmail.com>
2026-03-05 22:10:10 +0300

    View: Dashboard layout completed and errors fixed

---
+++ MiniGCS/Views/DashboardView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MiniGCS.Views
+{
+    /// <summary>
+    /// DashboardView.xaml için etkileşim mantığı
+    /// </summary>
+    public partial class DashboardView : UserControl
+    {
+        public DashboardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
